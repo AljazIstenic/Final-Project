79f1e15e64e9c4d8fcf4883105c62c3df3e05cb2
AljazIstenic <aljaz.istenic@gmail.com>
2017-06-11 23:55:58 +0200

    Added pipe operator, added ggplot

--- RScript.R
+++ RScript.R
@@ -11,10 +11,12 @@ Pokemon <- read_csv("~/GitHub/GroupProject/Pokemon.csv")
 View(Pokemon)
 
 Selected<-select(Pokemon,Name,Total,Generation,starts_with("Type"), Legendary)
-Filtered<-filter(Selected, Generation==1, !grepl('Mega', Name))
-Gen1<-select(Filtered, Name,Total,starts_with("Type"))
+Gen1<-filter(Selected, Generation==1, !grepl('Mega', Name))%>%select(Name,Total,starts_with("Type"))
 arrange(Gen1, desc(Total))
-Type<-group_by(Gen1, Type1)
+Type<-group_by(Gen1, Type1, Type2)
 Mean<-summarise(Type,avgT=mean(Total))
 arrange(Mean,desc(avgT))
-Mean
+filter(Gen1, Type2=='Ice')
+
+g <- ggplot(Gen1, aes(x=Type1, y=Total))+geom_point()
+g
