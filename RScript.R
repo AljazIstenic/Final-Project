3c14803335a58fc222ae1031808776c54d1ff924
AljazIstenic <aljaz.istenic@gmail.com>
2017-06-12 19:49:14 +0200

    Shortened with another pipe operator, added "|" to filter function in order to search for pokemon by both types

--- RScript.R
+++ RScript.R
@@ -10,13 +10,12 @@ library(readr)
 Pokemon <- read_csv("~/GitHub/GroupProject/Pokemon.csv")
 View(Pokemon)
 
-Selected<-select(Pokemon,Name,Total,Generation,starts_with("Type"), Legendary)
-Gen1<-filter(Selected, Generation==1, !grepl('Mega', Name))%>%select(Name,Total,starts_with("Type"))
+Gen1<-select(Pokemon,Name,Total,Generation,starts_with("Type"), Legendary)%>%filter(Generation==1, !grepl('Mega', Name))%>%select(Name,Total,starts_with("Type"))
 arrange(Gen1, desc(Total))
-Type<-group_by(Gen1, Type1, Type2)
+Type<-group_by(Gen1, Type1)
 Mean<-summarise(Type,avgT=mean(Total))
 arrange(Mean,desc(avgT))
-filter(Gen1, Type2=='Ice')
+filter(Gen1, Type1=='Ice' | Type2=='Ice')
 
 g <- ggplot(Gen1, aes(x=Type1, y=Total))+geom_point()
 g
